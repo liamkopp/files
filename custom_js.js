6132242498ef280e9c69499cf061213b61d0bf71
liamkopp <liam.r.kopp@gmail.com>
2021-04-07 23:58:10 -0400

    Add files via upload

---
+++ custom_js.js
@@ -0,0 +1,9 @@
+tippy('#svg_2', {
+				content: "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO'><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDckAuNVEBp21dz_N8eHWPx8V_OxhrgmWy5A&usqp=CAU'></a>",
+				allowHTML: true,
+				interactive: true,
+				positionFixed: true,
+				appendTo: document.body,
+				trigger: 'click',	
+				theme: 'light',
+			});
